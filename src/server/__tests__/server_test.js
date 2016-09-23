36a9f878a4b0441b42418fbcea032583d462aaa3
Pablo Miranda <pablolm@yahoo-inc.com>
2016-09-23 12:14:19 -0700

    adds /api/movies endpoint

--- src/server/__tests__/server_test.js
+++ src/server/__tests__/server_test.js
@@ -21,27 +21,55 @@ describe('src/server/index.js', function() {
             .then(done, done);
     });
 
-    it('launchs the UI through index.html', function(done) {
-        request(app)
-            .get('/')
-            .expect('Content-Type', 'text/html; charset=utf-8')
-            .expect('Content-Length', html.length.toString())
-            .expect(200)
-            .end(function(err, res) {
-                if (err) {
-                    done(err);
-                }
-                jsdom.env(res.text, function(e, window) {
-                    if (e) {
-                        done(e);
+    describe('/', function() {
+        it('launchs the UI through index.html', function(done) {
+            request(app)
+                .get('/')
+                .expect('Content-Type', 'text/html; charset=utf-8')
+                .expect('Content-Length', html.length.toString())
+                .expect(200)
+                .end(function(err, res) {
+                    if (err) {
+                        done(err);
                     }
+                    jsdom.env(res.text, function(e, window) {
+                        if (e) {
+                            done(e);
+                        }
 
-                    var appElement = window.document.body.children.app;
-                    
-                    expect(appElement.id).to.be.equal('app');
-                    done();
+                        var appElement = window.document.body.children.app;
+                        
+                        expect(appElement.id).to.be.equal('app');
+                        done();
+                    });
                 });
-            });
+        });    
+    });
+
+    describe('/api/movies', function() {
+
+        it('returns the list of movie assets', function(done) {
+            var jsonPayload = '';
+            request(app)
+                .get('/api/movies')
+                .expect('Content-Type', 'application/json')
+                .expect(200)
+                .end(function(err, res) {
+                    if(err) {
+                        done(err);
+                    }
+                    res.body.forEach(function(assetInfo) {
+                        expect(assetInfo).to.have.ownProperty('movieId');
+                        expect(assetInfo).to.have.ownProperty('movieName');
+                        expect(assetInfo).to.have.ownProperty('imageType');
+                        expect(assetInfo).to.have.ownProperty('thumbnailUrl');
+                        expect(assetInfo).to.have.ownProperty('fullSizeImageUrl');
+                        expect(assetInfo).to.have.ownProperty('languageCode');
+                    });
+                    done();
+                })
+        });
+
     });
 
 });
--- src/server/index.js
+++ src/server/index.js
@@ -1,10 +1,13 @@
 var express = require('express'),
+    fs = require('fs'),
     path = require('path'),
     app = express(),
     ExpressHandlebars = require('express-handlebars')
     handlebars = ExpressHandlebars.create({}),
     SRC_DIR = path.resolve(__dirname, '..'),
-    HTML_INDEX_PATH = path.join(SRC_DIR, 'static', 'index.hbs');
+    HTML_INDEX_PATH = path.join(SRC_DIR, 'static', 'index.hbs'),
+    JSON_PAYLOAD_PATH = path.join(SRC_DIR, 'server', 'stub', 'payload.json'),
+    STREAM_BUFFER_SIZE = 64 * 1024;
 
 // Renders a simple HTML to launch the UI
 app.get('/', function(req, res) {
@@ -13,6 +16,12 @@ app.get('/', function(req, res) {
             res.send(html);
         })
         .then(res.end, res.end);
+});
+
+app.get('/api/movies', function(req, res) {
+    res.set('Content-Type', 'application/json');
+    var stream = fs.createReadStream(JSON_PAYLOAD_PATH, { bufferSize: STREAM_BUFFER_SIZE});
+    stream.pipe(res);
 });
 
 module.exports = app;
